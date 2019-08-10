cb46806117e4d7a3d3f442f3f036806814434af7
Jamie <none>
2019-08-10 14:40:04 -0400

    init

---
+++ index.php
@@ -0,0 +1,25 @@
+<html>
+<head>
+<title>
+Jamie Yukes
+</title>
+<style>
+A:visited { color: #999999 }
+A:link { color: #999999 }
+body {
+	background: #000000;
+	font-family: Helvetica, Lucida;
+	color: #666666;
+}
+</style>
+</head>
+<body>
+<br/>
+<br/>
+<br/>
+<br/>
+<div align=center style="height:10000px;">
+<img src="/qrcode.png" border=0 alt="Jamie Yukes VCF contact information"/><br/>
+</div>
+</body>
+</html>
